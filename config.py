8fbfac467e0589e9d228be34799105f6f1be2c8f
Ngilja <ilungangilja3@gmail.com>
2026-03-01 12:42:27 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,35 @@
+# config.py
+TOKEN = "ICI_TON_TOKEN_TELEGRAM"  # 🔴 ON CHANGERA ÇA PLUS TARD
+
+TEMP_DIR = "temp"
+
+FORMATS_SUPPORTES = {
+    '.fdx': 'FDX Injector',
+    '.agn': 'AGN Injector',
+    '.jvc': 'JV Custom',
+    '.aip': 'ACE INYECTOR',
+    '.sks': 'SocksHTTP',
+    '.stk': 'Stark VPN Reloaded',
+    '.tnl': 'OpenTunnel',
+    '.ssh': 'SSH Injector',
+    '.nxp': 'NexPrime VPN',
+    '.purple': 'Purple VPN',
+    '.cln': '24Clan VPN Plus',
+    '.ziv': 'ZIVPN',
+    '.nm': 'NetMod VPN',
+    '.hat': 'HA Tunnel Plus',
+}
+
+TEXT_PROTOCOLS = {
+    'NETMOD': ['nm-vmess://', 'nm-vless://', 'nm-trojan://'],
+    'ARMOD': ['ar-vless://', 'ar-vmess://', 'ar-trojan://'],
+    'VMess': ['vmess://'],
+    'VLESS': ['vless://'],
+    'Trojan': ['trojan://'],
+}
+
+CONVERSIONS = {
+    '.fdx': ['.nxp', '.agn', '.sks', '.ssh', 'vmess://'],
+    '.nxp': ['.fdx', '.agn', '.sks', '.nm'],
+    '.agn': ['.fdx', '.nxp', '.sks', '.ssh'],
+}
